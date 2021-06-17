d23fc327e92dbc8c1b6352db0165835c914453aa
BididCoda <bididcoda2405@gmail.com>
2021-06-17 23:14:48 +0200

    plus de code js

--- scripts/echiquier.js
+++ scripts/echiquier.js
@@ -1,2 +1,13 @@
 let ouput = "";
 let size = 8;
+for( x = 0 ; x < size ; x++) {
+    for(y = 0 ; y < size ; y++) {
+        if((x+y) % 2 == 0) {
+            ouput += "";
+        }else {
+            ouput += "#"
+        }
+    }
+    ouput += "\n";
+}
+console.log(ouput);
