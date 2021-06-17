285e5d3b927834fde67b5f1f6360c36fe3387d8e
BididCoda <bididcoda2405@gmail.com>
2021-06-17 23:35:55 +0200

    Essaie autre technique d approche pour implementer chessboard

--- scripts/echiquier.js
+++ scripts/echiquier.js
@@ -3,7 +3,7 @@ let size = 8;
 for( x = 0 ; x < size ; x++) {
     for(y = 0 ; y < size ; y++) {
         if((x+y) % 2 == 0) {
-            ouput += "";
+            ouput += " ";
         }else {
             ouput += "#"
         }
@@ -11,3 +11,13 @@ for( x = 0 ; x < size ; x++) {
     ouput += "\n";
 }
 console.log(ouput);
+
+let board = "";
+for(let a = 0 ; a < 8 ; a++) {
+    for(let b = 0 ; b < 8 ; 8++) {
+        board += (a % 2) == (b % 2) ? " " : "#";
+    }
+    board += "\n";
+}
+
+console.log(board);
